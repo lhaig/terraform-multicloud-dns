f900a53c396a783ed1a71f912ab0f67e2705cafc
Robert Peteuil <robert@peteuil.com>
2020-07-25 12:10:51 -0700

    remove provider blocks from module

--- main.tf
+++ main.tf
@@ -1,17 +1,3 @@
-# AWS General Configuration
-provider "aws" {
-  version = "~> 2.46.0"
-  region  = var.aws_region
-}
-
-# GCP General Configuration
-provider "google" {
-  version = "~> 3.5"
-  project = var.gcp_project
-  region  = var.gcp_region
-}
-
-# Azure General Configuration
-provider "azurerm" {
-  version = "~>1.42.0"
+terraform {
+  required_version = ">= 0.12.4"
 }
