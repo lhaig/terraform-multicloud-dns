4da2e2b8b0dc256e1049debc08ddf71899535785
Robert Peteuil <robert@peteuil.com>
2020-07-25 04:12:10 -0700

    fix owner label on gcp (#4)

--- gcp.tf
+++ gcp.tf
@@ -8,7 +8,7 @@ resource "google_dns_managed_zone" "gcp_sub_zone" {
   description = "Managed by Terraform, Delegated Sub Zone for GCP for  ${var.namespace}"
   labels = {
     name       = var.namespace
-    owner      = var.owner
+    owner      = replace(var.owner, "/@|\\./", "-")
     created-by = var.created-by
   }
 }
